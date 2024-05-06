02763298f9738ebf7d759a5555fed08d47b4a27d
bolfosu <bolfamihail@gmail.com>
2024-05-06 22:26:12 +0200

    a method to read from the file

--- FileData/DataContainer.cs
+++ FileData/DataContainer.cs
@@ -6,4 +6,42 @@ public class DataContainer
 {
     public ICollection<User> Users { get; set; }
     public ICollection<Todo> Todos { get; set; }
+
+    private const string filePath = "data.json";
+    private DataContainer? dataContainer;
+
+    public ICollection<Todo> Todos
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Todos;
+        }
+    }
+
+    public ICollection<User> Users
+    {
+        get
+        {
+            LoadData();
+            return dataContainer!.Users;
+        }
+    }
+
+    private void LoadData()
+    {
+        if (dataContainer != null) return;
+
+        if (!File.Exists(filePath))
+        {
+            dataContainer = new()
+            {
+                Todos = new List<Todo>(),
+                Users = new List<User>()
+            };
+            return;
+        }
+        string content = File.ReadAllText(filePath);
+        dataContainer = JsonSerializer.Deserialize<DataContainer>(content);
+    }
 }
