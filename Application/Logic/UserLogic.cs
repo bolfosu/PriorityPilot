1cf9ffe4a0048aef3ba08c3421d3c2270db0f839
bolfosu <bolfamihail@gmail.com>
2024-05-12 19:39:41 +0200

    implement the IUserLogic interface and changed IUserDao to public

--- Application/Logic/UserLogic.cs
+++ Application/Logic/UserLogic.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Application.DaoInterfaces;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static UserCreationDto;
 
 namespace Application.Logic
 {
-    internal class UserLogic
+    public class UserLogic : IUserLogic
     {
+        private readonly IUserDao userDao;
+
+        public UserLogic(IUserDao userDao)
+        {
+            this.userDao = userDao;
+        }
+
+        public Task<User> CreateAsync(UserCreationDto userToCreate)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
