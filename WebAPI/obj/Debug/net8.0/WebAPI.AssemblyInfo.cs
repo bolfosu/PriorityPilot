0b128027b880df7baf035bf2e54798390b349cf7
bolfosu <bolfamihail@gmail.com>
2024-05-14 20:52:45 +0200

    created Controllers directory and UserController class

--- WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd3127b1f0aec68f1013d756ae653fcb65cfe50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24900a1de93dfd1451825d84b53b0204e925d008")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
