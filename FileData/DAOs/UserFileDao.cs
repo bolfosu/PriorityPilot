ab6a58e15d845db944e999889059626790686f12
bolfosu <bolfamihail@gmail.com>
2024-05-15 12:10:28 +0200

    create user method

--- FileData/DAOs/UserFileDao.cs
+++ FileData/DAOs/UserFileDao.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Application.DaoInterfaces;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,36 @@ using System.Threading.Tasks;
 
 namespace FileData.DAOs
 {
-    internal class UserFileDao
+    public class UserFileDao : IUserDao
     {
+        private readonly FileContext context;
+
+        public UserFileDao(FileContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<User> CreateAsync(User user)
+        {
+
+            int userId = 1;
+            if (context.Users.Any())
+            {
+                userId = context.Users.Max(u => u.Id);
+                userId++;
+            }
+
+            user.Id = userId;
+
+            context.Users.Add(user);
+            context.SaveChanges();
+
+            return Task.FromResult(user);
+        }
+
+        public Task<User?> GetByUsernameAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
