53bee968a28b0979492d68946801b9f37b0296e6
bolfosu <bolfamihail@gmail.com>
2024-05-16 21:15:26 +0200

    save changes method

--- FileData/FileContext.cs
+++ FileData/FileContext.cs
@@ -1,6 +1,16 @@
-﻿namespace FileData;
+﻿using System.Text.Json;
+
+namespace FileData;
 
 public class FileContext
 {
-            
+    public void SaveChanges()
+    {
+        string serialized = JsonSerializer.Serialize(dataContainer, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        });
+        File.WriteAllText(filePath, serialized);
+        dataContainer = null;
+    }
 }
