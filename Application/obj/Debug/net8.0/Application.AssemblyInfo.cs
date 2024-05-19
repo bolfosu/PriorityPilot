0fd94645346dd85cbcaff12080397fc2a4d5edf4
bolfosu <bolfamihail@gmail.com>
2024-05-19 10:37:16 +0200

    create UserLogic interface

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53bee968a28b0979492d68946801b9f37b0296e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6d85f9df0682b9f604a217f821d64c7f8e1430a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Domain/DTOs/SearchUserParametersDto.cs
+++ Domain/DTOs/SearchUserParametersDto.cs
@@ -8,5 +8,12 @@ namespace Domain.DTOs
 {
     internal class SearchUserParametersDto
     {
+
+        public string? UsernameContains { get; }
+
+        public SearchUserParametersDto(string? usernameContains)
+        {
+            UsernameContains = usernameContains;
+        }
     }
 }
