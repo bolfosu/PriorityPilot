c8b5c686eb4e6a5978aa3acb8ed79da90da83527
bolfosu <bolfamihail@gmail.com>
2024-05-20 22:10:05 +0200

    define the method in the IUserDao interface

--- Application/DaoInterfaces/IUserDao.cs
+++ Application/DaoInterfaces/IUserDao.cs
@@ -11,6 +11,7 @@ namespace Application.DaoInterfaces
     {
         Task<User> CreateAsync(User user);
         Task<User?> GetByUsernameAsync(string userName);
+        public Task<IEnumerable<User>> GetAsync(SearchUserParametersDto searchParameters);
     }
 
     
