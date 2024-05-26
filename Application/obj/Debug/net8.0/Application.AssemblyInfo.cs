6472b17e81eecb1e19c82294dacc480e4e85eded
bolfosu <bolfamihail@gmail.com>
2024-05-26 20:37:09 +0200

    create ITodoDao interface

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+131890faaa7d947ef19da257b94a797fb17fdeaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9dbdcfb5bdf02d6c09a1ab5574f669218feadaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
