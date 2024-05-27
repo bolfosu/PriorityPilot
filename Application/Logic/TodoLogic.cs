7044acd67ab1e5f18a3d9c83527ba570c2de3968
bolfosu <bolfamihail@gmail.com>
2024-05-27 23:00:05 +0200

    todo validation and creation methods

--- Application/Logic/TodoLogic.cs
+++ Application/Logic/TodoLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,35 @@ using System.Threading.Tasks;
 
 namespace Application.Logic
 {
-    internal class TodoLogic
+    public class TodoLogic : ITodoLogic
     {
+        private readonly ITodoDao todoDao;
+        private readonly IUserDao userDao;
+
+        public TodoLogic(ITodoDao todoDao, IUserDao userDao)
+        {
+            this.todoDao = todoDao;
+            this.userDao = userDao;
+        }
+
+        public async Task<Todo> CreateAsync(TodoCreationDto dto)
+        {
+            User? user = await userDao.GetByIdAsync(dto.OwnerId);
+            if (user == null)
+            {
+                throw new Exception($"User with id {dto.OwnerId} was not found.");
+            }
+
+            ValidateTodo(dto);
+            Todo todo = new Todo(user, dto.Title);
+            Todo created = await todoDao.CreateAsync(todo);
+            return created;
+        }
+
+        private void ValidateTodo(TodoCreationDto dto)
+        {
+            if (string.IsNullOrEmpty(dto.Title)) throw new Exception("Title cannot be empty.");
+            // other validation stuff
+        }
     }
 }
