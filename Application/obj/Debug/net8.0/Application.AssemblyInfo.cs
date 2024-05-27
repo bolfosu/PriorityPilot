895b9dac90f0500ed627c3269d72651b0189bbab
bolfosu <bolfamihail@gmail.com>
2024-05-27 22:57:43 +0200

    created TodoLogic class

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9dbdcfb5bdf02d6c09a1ab5574f669218feadaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6472b17e81eecb1e19c82294dacc480e4e85eded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
