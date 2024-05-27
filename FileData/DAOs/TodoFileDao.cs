f7319e0cf0e98461d2928a68c4d891b889c43b47
bolfosu <bolfamihail@gmail.com>
2024-05-27 23:23:35 +0200

    fixed TodosController errors

--- FileData/DAOs/TodoFileDao.cs
+++ FileData/DAOs/TodoFileDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Domain;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace FileData.DAOs
 {
-    internal class TodoFileDao
+    public class TodoFileDao : ITodoDao
     {
+        private readonly FileContext context;
+
+        public TodoFileDao(FileContext context)
+        {
+            this.context = context;
+        }
+
+        public Task<Todo> CreateAsync(Todo todo)
+        {
+            int id = 1;
+            if (context.Todos.Any())
+            {
+                id = context.Todos.Max(t => t.Id);
+                id++;
+            }
+
+            todo.Id = id;
+
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return Task.FromResult(todo);
+        }
     }
 }
