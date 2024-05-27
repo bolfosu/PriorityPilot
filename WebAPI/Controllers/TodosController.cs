8a10e99eafc8f6d527b82a3460277d3c93e8eee6
bolfosu <bolfamihail@gmail.com>
2024-05-27 23:26:14 +0200

    created TodosController

---
+++ WebAPI/Controllers/TodosController.cs
@@ -0,0 +1,36 @@
+﻿using Application.Logic;
+using Application.LogicInterfaces;
+using Domain;
+using Domain.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TodosController : ControllerBase
+{
+
+    private readonly ITodoLogic todoLogic;
+
+    public TodosController(ITodoLogic todoLogic)
+    {
+        this.todoLogic = todoLogic;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Todo>> CreateAsync(TodoCreationDto dto)
+    {
+        try
+        {
+            Todo created = await todoLogic.CreateAsync(dto);
+            return Created($"/todos/{created.Id}", created);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+            return StatusCode(500, e.Message);
+        }
+    }
+}
