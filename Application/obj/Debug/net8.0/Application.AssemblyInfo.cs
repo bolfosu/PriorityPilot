559da67cb05d9482e59e7a75b02fc12b659d0b56
bolfosu <bolfamihail@gmail.com>
2024-06-09 22:14:23 +0100

    created delete todo method

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e44b5fac058e6a72421d3f7abda978589befdfe")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a86ce05c5d3d7f88d2d6bee5aa9c2f368a216b68")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
